523ab81910369699a33ae8650e8e4e465a7a4d21
Tecsi Aron <aron@edituraedu.ro>
2024-07-15 13:21:46 +0300

    Test setup complete (all parse tests passing)

--- tests/ParseTest.php
+++ tests/ParseTest.php
@@ -1,9 +1,21 @@
 <?php
 
 
+use EdituraEDU\UBLRenderer\XMLReaderProvider;
 use PHPUnit\Framework\TestCase;
 
 class ParseTest extends TestCase
 {
-
+    public function testUBLParsing():void
+    {
+        $classes= XMLReaderProvider::CLASSES;
+        for($i=0; $i<sizeof($classes); $i++)
+        {
+            $testFunction=[$classes[$i], "TestParse"];
+            $reason="";
+            $testResult= $testFunction($reason);
+            $this->assertTrue($testResult, "{$classes[$i]}::TestParse failed: $reason");
+            echo "{$classes[$i]}::TestParse passed\n";
+        }
+    }
 }
---
+++ tests/bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+echo "Bootstrapping tests";
+require_once dirname(__FILE__) . "/../vendor/autoload.php";
