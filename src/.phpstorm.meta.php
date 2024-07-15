4d12db2894b527ff8f253447c002ab90f1da9d95
Tecsi Aron <aron@edituraedu.ro>
2024-07-15 16:18:43 +0300

    Not sure if this is needed

---
+++ src/.phpstorm.meta.php
@@ -0,0 +1,6 @@
+<?php
+namespace PHPSTORM_META {
+    override(\Twig\Environment::render(0), map([
+        'invoice' => '@EdituraEDU\UBLRenderer\UBLObjectDefinitions\ParsedUBLInvoice@',
+    ]));
+}
