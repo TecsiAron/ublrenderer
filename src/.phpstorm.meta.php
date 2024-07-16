786c02fb4850f780d4d6ac5d25a1d9803ca4ff42
Tecsi Aron <aron@edituraedu.ro>
2024-07-16 11:57:40 +0300

    Add license/copyright notice to source files

--- src/.phpstorm.meta.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace PHPSTORM_META {
-    override(\Twig\Environment::render(0), map([
-        'invoice' => '@EdituraEDU\UBLRenderer\UBLObjectDefinitions\ParsedUBLInvoice@',
-    ]));
-}
