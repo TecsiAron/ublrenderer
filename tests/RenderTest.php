a0de0a6782cd968e4f91b20ce23fcb926e50659c
Tecsi Aron <aron@edituraedu.ro>
2024-07-16 11:48:07 +0300

    Rework render test

--- tests/RenderTest.php
+++ tests/RenderTest.php
@@ -8,7 +8,30 @@ class RenderTest extends TestCase
 {
     public function testRender()
     {
+        if(file_exists("test_render.html"))
+        {
+            @unlink("test_render.html");
+        }
+        $this->assertFileDoesNotExist("test_render.html");
         $content= ParsedUBLInvoice::GetTestXML();
         $renderer = new UBLRenderer($content);
+        try
+        {
+            $html = $renderer->CreateHTML();
+        }
+        catch (Exception $e)
+        {
+            $this->fail($e->getMessage());
+        }
+        $this->assertNotEmpty($html);
+        try
+        {
+            file_put_contents("test_render.html", $html);
+        }
+        catch (Exception $e)
+        {
+            $this->fail($e->getMessage());
+        }
+        $this->assertFileExists("test_render.html");
     }
 }
