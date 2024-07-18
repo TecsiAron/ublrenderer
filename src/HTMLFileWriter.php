0690d4c0e5b2eb6e63998638ecdaafb9443e34b0
Tecsi Aron <aron@edituraedu.ro>
2024-07-18 13:15:51 +0300

    Add support for $path default value

--- src/HTMLFileWriter.php
+++ src/HTMLFileWriter.php
@@ -23,13 +23,42 @@ use EdituraEDU\UBLRenderer\UBLObjectDefinitions\ParsedUBLInvoice;
 
 class HTMLFileWriter implements IInvoiceWriter
 {
+
     private string $Path;
-    public function __construct(string $path)
+
+    /**
+     * If path does not have a file name, it will be generated based on the invoice id
+     * @param string|null $path if null dirname(__FILE__)."/../output/<invoice_id>.html will be used"
+     */
+    public function __construct(?string $path=null)
     {
+        if($path==null)
+        {
+            $path = dirname(__FILE__)."/../output/";
+        }
         $this->Path = $path;
     }
-    function WriteContent(string $hmlContent, ParsedUBLInvoice $invoice): void
+    public function WriteContent(string $hmlContent, ParsedUBLInvoice $invoice): void
     {
+        $path=$this->Path;
+        if(is_dir($this->Path))
+        {
+            if(!str_ends_with($path, PATH_SEPARATOR))
+            {
+                $path.=PATH_SEPARATOR;
+            }
+            $path.=$invoice->ID.".html";
+            $this->Path = $path;
+        }
         file_put_contents($this->Path, $hmlContent);
+    }
+
+    /**
+     * Might be different from path specified in the constructor, check the constructor docs for more details
+     * @return string
+     */
+    public function GetPath(): string
+    {
+        return $this->Path;
     }
 }
